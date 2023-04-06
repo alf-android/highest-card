e22bed0864f04314389bc1b9c3d007add4d2232d
Aitor Lagunas <alagunas@shotl.com>
2023-04-06 17:21:49 +0200

    Add gradle KTS

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.alagunas.highestcard"
+        minSdk = 23
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            setProguardFiles(
+                listOf(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = rootProject.extra["composeVersion"] as String
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    /*implementation(project(":usecases"))
+    implementation(project(":data"))
+    implementation(project(":domain"))
+    implementation(project(":common"))
+*/
+    implementation("androidx.compose.material3:material3:${rootProject.extra["composeMaterial3Version"]}")
+//    implementation("androidx.compose.material3:material3-window-size-class:1.0.1")
+    implementation("androidx.compose.material:material:${rootProject.extra["composeMaterialVersion"]}")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${rootProject.extra["lifecycleVersion"]}")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$${rootProject.extra["lifecycleVersion"]}")
+
+    implementation("androidx.compose.ui:ui:${rootProject.extra["composeVersion"]}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["composeVersion"]}")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["composeVersion"]}")
+    debugImplementation("androidx.compose.ui:ui-tooling:${rootProject.extra["composeVersion"]}")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:${rootProject.extra["composeVersion"]}")
+
+    implementation("androidx.navigation:navigation-compose:${rootProject.extra["navigationVersion"]}")
+    implementation("androidx.navigation:navigation-fragment-ktx:${rootProject.extra["navigationVersion"]}")
+    implementation("androidx.navigation:navigation-ui-ktx:${rootProject.extra["navigationVersion"]}")
+
+    testImplementation("junit:junit:${rootProject.extra["junitVersion"]}")
+    androidTestImplementation("androidx.test.ext:junit:${rootProject.extra["testJunitVersion"]}")
+    androidTestImplementation("androidx.test.espresso:espresso-core:${rootProject.extra["testEspressoVersion"]}")
+
+    implementation("io.coil-kt:coil-compose:${rootProject.extra["coilVersion"]}")
+    implementation("com.google.code.gson:gson:${rootProject.extra["gsonVersion"]}")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+
+buildscript {
+
+    extra.apply {
+        //UI
+        set("lifecycleVersion", "2.5.1")
+        set("navigationVersion", "2.5.3")
+        set("composeVersion", "1.2.0")
+        set("composeMaterialVersion", "1.2.1")
+        set("composeMaterial3Version", "1.0.0-alpha01")
+        set("coilVersion", "2.2.2")
+        //DB
+        set("roomVersion", "2.4.3")
+        //API
+        set("okhttpVersion", "4.9.3")
+        set("retrofitVersion", "2.9.0")
+        set("gsonVersion", "2.9.0")
+        set("gsonConverterVersion", "2.9.0")
+        //DI
+        set("koinVersion", "3.2.0")
+        //Test
+        set("junitVersion", "4.13.2")
+        set("testJunitVersion", "1.1.5")
+        set("testEspressoVersion", "3.5.1")
+    }
+
+    repositories {
+        maven("https://maven.google.com")
+        mavenCentral()
+        google()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.neenbedankt.gradle.plugins:android-apt:1.8")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+    }
+}
+
+allprojects {
+
+}
+
+tasks.register<Delete>("clean").configure {
+    delete(rootProject.buildDir)
+}
