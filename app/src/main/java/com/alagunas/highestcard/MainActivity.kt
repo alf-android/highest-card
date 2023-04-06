fc95783a0cffdc8a51f4748f1058cc17940fa535
Aitor Lagunas <alagunas@shotl.com>
2023-04-06 16:54:00 +0200

    Add project

---
+++ app/src/main/java/com/alagunas/highestcard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alagunas.highestcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
