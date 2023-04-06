736d894dae080edb0b622e1b1de1372748271e20
Aitor Lagunas <alagunas@shotl.com>
2023-04-06 23:44:44 +0200

    feat: init clean architecture

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ buildscript {
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
     }
 }
